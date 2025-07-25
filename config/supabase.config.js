8c89d8daf01bc9dbcea587a3e51c1190a2b1010c
yashwanth535 <yashwanth.lumia535@gmail.com>
2025-07-25 16:15:34 +0530

    working upload functionality

--- config/supabase.config.js
+++ config/supabase.config.js
@@ -1,6 +1,8 @@
 const { createClient } = require("@supabase/supabase-js");
+const dotenv = require("dotenv")
+dotenv.config();
 
-const supabaseUrl = "https://bkdbwbndgtxnsvrlvept.supabase.co"; // Replace with your Supabase URL
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJrZGJ3Ym5kZ3R4bnN2cmx2ZXB0Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc1MzM2ODEyNywiZXhwIjoyMDY4OTQ0MTI3fQ.Nl9yENQalc4Lp5XtES6TiOaAGlQHLEefqdBMzJQcHA0";
+const supabaseUrl = process.env.supabase_url;
+const supabaseKey = process.env.supabase_service_secret;//bypasses rls
 const supabase = createClient(supabaseUrl, supabaseKey);
 module.exports = supabase;
